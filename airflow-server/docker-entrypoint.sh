c4146ae32e103c3d6230afc91c39fb50aba37b9e
malcolndandaro <dmalcoln@gmail.com>
2020-12-28 18:55:35 -0300

    add requirements.txt

--- airflow-server/docker-entrypoint.sh
+++ airflow-server/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -m 
+pip3 install --no-cache-dir -r /app/airflow/requirements.txt
 airflow db init
 airflow users create --username admin --password admin --role Admin --email placeholder@gmail.com --firstname admin --lastname admin
 airflow webserver --port 8080 &
